6d901952ff7ea1438e5bb541fa0f220554a008a0
efayamb <ambrus.fay@ericsson.com>
2025-10-21 16:28:29 +0200

    js and css trial again

--- local/table_demo/index.php
+++ local/table_demo/index.php
@@ -11,7 +11,8 @@ $PAGE->set_heading(get_string('title', 'local_table_demo'));
 
 $PAGE->requires->js(new moodle_url('https://cdn.datatables.net/2.3.4/js/dataTables.min.js'));
 $PAGE->requires->css(new moodle_url('https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.min.css'));
+$PAGE->requires->js_call_amd('local_table_demo/main', 'init');
 
 echo $OUTPUT->header();
-$OUTPUT->render_from_template();
+$OUTPUT->render_from_template('local_table_demo/main_page', []);
 echo $OUTPUT->footer();
