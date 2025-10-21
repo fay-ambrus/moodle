40d68d5d8a31f8a87e5b35bebca6572c55a984e5
efayamb <ambrus.fay@ericsson.com>
2025-10-21 17:32:51 +0200

    Auto commit from script 10/21/2025 17:32:42

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,8 @@
 $LocalBranch = "MOODLE_311_STABLE"                  # your local branch
 $RemoteName = "origin"                              # your fork or main repo
 $ServerUser = "cloud"                               # SSH username
-$ServerHost = "vm.smallville.cloud.bme.hu:6906"     # server address
+$ServerHost = "vm.smallville.cloud.bme.hu"          # server address
+$ServerPort = "6906"                                # SSH port
 $ServerPath = "/var/www/html/moodle"                # path to Moodle on server
 $CommitMessage = if ($args.Length -gt 0) { $args[0] } else { "Auto commit from script $(Get-Date)" }
 
@@ -30,6 +31,6 @@ git push $RemoteName $LocalBranch
 # STEP 2: SSH into server and pull
 # -------------------------------
 Write-Host "Connecting to server and pulling changes..."
-ssh $ServerUser@$ServerHost -p 6906 "cd $ServerPath && git fetch --all && git reset --hard $RemoteName/$LocalBranch && php admin/cli/purge_caches.php"
+ssh $ServerUser@$ServerHost -p $ServerPort "cd $ServerPath && git fetch --all && git reset --hard $RemoteName/$LocalBranch && php admin/cli/purge_caches.php"
 
 Write-Host "Done!"
