4125fb835273ae5f94264bfdf77f878d1837d91d
efayamb <ambrus.fay@ericsson.com>
2025-10-21 16:49:11 +0200

    sdf

--- local/table_demo/main.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-    return {
-        init: function() {
-            document.getElementById('demo_button').addEventListener('click', () => {
-                alert('Action!');
-            });
-        }
-    };
-});
