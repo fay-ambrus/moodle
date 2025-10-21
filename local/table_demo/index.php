e32b66f7e753efaaf9c3ecae79ab4b1e09e8f0fa
efayamb <ambrus.fay@ericsson.com>
2025-10-21 16:44:43 +0200

    sd

--- local/table_demo/index.php
+++ local/table_demo/index.php
@@ -11,8 +11,8 @@ $PAGE->set_heading(get_string('title', 'local_table_demo'));
 
 $PAGE->requires->js(new moodle_url('https://cdn.datatables.net/2.3.4/js/dataTables.min.js'));
 $PAGE->requires->css(new moodle_url('https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.min.css'));
-$PAGE->requires->js_call_amd('local/table_demo/main.js', 'init');
+$PAGE->requires->js_call_amd('local_table_demo/main', 'init');
 
 echo $OUTPUT->header();
-$OUTPUT->render_from_template('local/table_demo/templates/main_page', []);
+echo $OUTPUT->render_from_template('local_table_demo/main_page', []);
 echo $OUTPUT->footer();
