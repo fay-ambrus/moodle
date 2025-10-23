dadb299ba8223dbbac04e7181990d59d9ccc2ba4
efayamb <ambrus.fay@ericsson.com>
2025-10-23 11:41:27 +0200

    Auto commit from script 10/23/2025 11:41:01

--- local/table_demo/amd/src/main.js
+++ local/table_demo/amd/src/main.js
@@ -1,4 +1,10 @@
-define(['datatables.net-dt'], function() {
+requirejs.config({
+    paths: {
+        datatables: 'https://cdn.datatables.net/2.3.4/js/dataTables.min.js'
+    }
+});
+
+define(['datatables'], function() {
     return {
         init: function() {
             document.getElementById('demo_button').addEventListener('click', () => {
