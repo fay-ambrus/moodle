465540d1f8b216dc6e868bc4e96ca8ee1872d43f
efayamb <ambrus.fay@ericsson.com>
2025-10-23 11:04:26 +0200

    Auto commit from script 10/23/2025 11:04:01

--- local/table_demo/amd/src/main.js
+++ local/table_demo/amd/src/main.js
@@ -1,7 +1,8 @@
-define([], function() {
+define(['my-module'], function() {
     return {
         init: function() {
             console.log('JS init');
+            console.log('sample change');
             document.getElementById('demo_button').addEventListener('click', () => {
                 alert('Action!');
             });
