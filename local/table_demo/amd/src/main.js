f6a597f4e507e66d9df9fe7112ef85af03cd97c6
efayamb <ambrus.fay@ericsson.com>
2025-10-23 11:50:21 +0200

    Auto commit from script 10/23/2025 11:50:11

--- local/table_demo/amd/src/main.js
+++ local/table_demo/amd/src/main.js
@@ -1,10 +1,21 @@
-define(['core/datatables'], function() {
+requirejs.config({
+    paths: {
+        datatables: 'https://cdn.datatables.net/2.3.4/js/dataTables.min.js'
+    },
+    shim: {
+        datatables: {
+            exports: 'D'
+        }
+    }
+});
+
+define(['datatables'], function() {
     return {
         init: function() {
             document.getElementById('demo_button').addEventListener('click', () => {
                 alert('Action!');
             });
-            let table = new DataTable('#myTable');
+            let table = new D.DataTable('#myTable');
         }
     };
 });
