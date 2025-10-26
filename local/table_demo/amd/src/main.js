808e1d3613034684909eb9b0843c6654cc04d9ed
efayamb <ambrus.fay@ericsson.com>
2025-10-26 13:19:30 +0100

    Auto commit from script 10/26/2025 13:19:20

--- local/table_demo/amd/src/main.js
+++ local/table_demo/amd/src/main.js
@@ -16,7 +16,7 @@ define(['datatables'], function() {
             $('#myTable').DataTable({
                 initComplete: function () {
                     console.log("init complete ");
-                    let groupColumn = this.api().column(-1).select();
+                    let groupColumn = this.api().column(3).select();
 
                     let title = groupColumn.footer().textContent;
 
