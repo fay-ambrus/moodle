83c706bfff990bb294e23f9175408194dd534d2b
efayamb <ambrus.fay@ericsson.com>
2025-11-04 16:51:33 +0100

    Auto commit from script 11/04/2025 16:51:29

--- local/table_demo/amd/src/main.js
+++ local/table_demo/amd/src/main.js
@@ -1,6 +1,6 @@
 requirejs.config({
     paths: {
-        datatables: 'https://cdn.datatables.net/2.3.4/js/jquery.dataTables.min',
+        datatables: 'https://cdn.datatables.net/2.3.4/js/dataTables.min',
         datatables_select: 'https://cdn.datatables.net/select/3.1.3/js/dataTables.select.min'
     },
     shim: {
