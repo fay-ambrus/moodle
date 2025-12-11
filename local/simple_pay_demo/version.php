bf6e79b82c3315d256027f703fde23902cdcc751
efayamb <ambrus.fay@ericsson.com>
2025-12-11 06:01:40 +0100

    Auto commit from script 12/11/2025 06:01:31

--- local/simple_pay_demo/version.php
+++ local/simple_pay_demo/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'simple_pay_demo'; // Full name of the plugin.
-$plugin->version   = 2025120900;	    // YYYYMMDDXX (date + increment).
-$plugin->requires  = 2021051718;		// Minimum Moodle version.
+$plugin->component = 'simple_pay_demo';     // Full name of the plugin.
+$plugin->version   = 2025120900;		    // YYYYMMDDXX (date + increment).
+$plugin->requires  = 2021051718;			// Minimum Moodle version.
