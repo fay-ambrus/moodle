909825bf8d83644420eac39a5f3e30f24bb655ab
efayamb <ambrus.fay@ericsson.com>
2025-12-11 07:32:53 +0100

    Auto commit from script 12/11/2025 07:32:48

--- local/simple_pay_demo/index.php
+++ local/simple_pay_demo/index.php
@@ -1,4 +1,6 @@
 <?php
+
+use core\check\performance\debugging;
 require('../../config.php');
 require_once(__DIR__.'/src/config.php');
 require_once(__DIR__.'/src/SimplePayV21.php');
@@ -57,6 +59,11 @@ if (empty($USER->country) || empty($USER->city) || empty($USER->address)) {
     }
 
     else if ($data = $form->get_data()) {
+        debugging($data->country);
+        debugging($data->city);
+        debugging($data->address);
+        debugging($data->zip);
+
         $trx->addGroupData('invoice', 'country', $data->country);
         $trx->addGroupData('invoice', 'state', $data->state);
         $trx->addGroupData('invoice', 'city', $data->city);
