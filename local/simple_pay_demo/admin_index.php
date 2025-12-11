cbdf8e975ffd10e0ca28ac14071c1dccaf8ea1ff
efayamb <ambrus.fay@ericsson.com>
2025-12-11 06:06:36 +0100

    Auto commit from script 12/11/2025 06:06:31

--- local/simple_pay_demo/admin_index.php
+++ local/simple_pay_demo/admin_index.php
@@ -5,7 +5,7 @@ require_login();
 $context = context_system::instance();
 
 $PAGE->set_context($context);
-$PAGE->set_url(new moodle_url('/local/simple_pay_demo/pages/manage.php'));
+$PAGE->set_url(new moodle_url('/local/simple_pay_demo/admin_index.php'));
 $PAGE->set_title('Manage Something');
 $PAGE->set_heading('Manage Something');
 
